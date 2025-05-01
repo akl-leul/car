1968fd59ceb9e823abf1f09e8f19e9806c2057df
Leul Ayfokru <abateisking@gmail.com>
2025-05-01 09:05:36 +0000

    UPDATE

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -4,7 +4,7 @@ import { Facebook, Instagram, Twitter } from 'lucide-react'; // optional icons
 export default function Footer() {
   return (
     <footer className="bg-[#0f0f0f] text-gray-400 px-6 py-10  ">
-      <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-3 gap-8">
+      <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-4 gap-6">
 
         {/* Brand */}
         <div>
@@ -14,12 +14,20 @@ export default function Footer() {
 
         {/* Links */}
         <div className="flex flex-col space-y-2">
-          <h2 className="text-gray-500">Links</h2>
+          <h2 className="text-white text-xl font-extrabold">Links</h2>
           <a href="/cars" className="footer-link">Cars</a>
-          <a href="#about" className="footer-link">About Us</a>
-          <a href="#contact" className="footer-link">Contact</a>
+          <a href="/about" className="footer-link">About Us</a>
+          <a href="/contact" className="footer-link">Contact</a>
           <a href="/privacy" className="footer-link">Privacy Policy</a>
         </div>
+        <div className="flex flex-col space-y-2">
+  <h2 className="text-white text-xl font-extrabold">Luxury Brands</h2>
+  <a href="/cars" className="footer-link">Lambohrgini</a>
+  <a href="/cars" className="footer-link">BMW</a>
+  <a href="/cars" className="footer-link">Dodge</a>
+  <a href="/cars" className="footer-link">Porsche</a>
+  <a href="/cars" className="footer-link">Rolls-Royce</a>
+</div>
 
         {/* Socials */}
         <div className="flex md:justify-end space-x-4 mt-4 md:mt-0">
@@ -27,6 +35,8 @@ export default function Footer() {
           <a href="#" aria-label="Instagram" className="hover:text-white transition"><Instagram size={20} /></a>
           <a href="#" aria-label="Twitter" className="hover:text-white transition"><Twitter size={20} /></a>
         </div>
+
+        
       </div>
 
       <div className="mt-10 border-t border-gray-700 pt-4 text-center text-sm">
