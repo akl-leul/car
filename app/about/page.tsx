67c4816c4e940d8722b6be1e04a0bf22c22ba68f
Leul Ayfokru <abateisking@gmail.com>
2025-05-02 21:31:27 +0000

    UPDATE

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -30,7 +30,7 @@ export default function About() {
       <div className="relative isolate overflow-hidden bg-gray-900 py-24 sm:py-32">
         <Image
           alt=""
-          src="https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-y=.8&w=2830&h=1500&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply"
+          src="/car.png"
           fill
           className="absolute inset-0 -z-10 object-cover object-center opacity-70"
           style={{ backgroundAttachment: 'fixed' }}
